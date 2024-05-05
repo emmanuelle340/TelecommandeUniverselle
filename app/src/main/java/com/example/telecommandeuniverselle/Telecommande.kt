5276f8e39993828ba5a0290824fa6c9da7fcd667
emmanuelle340 <emmanuellekenmogne340@gmail.com>
2024-05-05 19:50:40 +0100

    commit final, changement du fichier json, et envoie des frequence

--- app/src/main/java/com/example/telecommandeuniverselle/Telecommande.kt
+++ app/src/main/java/com/example/telecommandeuniverselle/Telecommande.kt
@@ -2,20 +2,31 @@ package com.example.telecommandeuniverselle
 
 import android.hardware.ConsumerIrManager
 import android.content.Context
+import androidx.compose.material3.Text
 
 class Telecommande(context: Context) {
 
-    private var irManager: ConsumerIrManager = context.getSystemService(Context.CONSUMER_IR_SERVICE) as ConsumerIrManager
+    private var irManager: ConsumerIrManager? = null
+
+    init {
+        try {
+            irManager = context.getSystemService(Context.CONSUMER_IR_SERVICE) as? ConsumerIrManager
+        } catch (e: Exception) {
+            // Gérer l'exception ici
+            e.printStackTrace()
+
+        }
+    }
 
     // Vérifie si le périphérique dispose d'un émetteur infrarouge
-    fun hasIrEmitter(): Boolean {
-        return irManager.hasIrEmitter()
+    fun hasIrEmitter(): Boolean? {
+        return irManager?.hasIrEmitter()
     }
 
     // Envoie un signal infrarouge
     fun sendIrSignal(frequency: Int, pattern: IntArray) {
-        if (hasIrEmitter()) {
-            irManager.transmit(frequency, pattern)
+        if (hasIrEmitter() == true) {
+            irManager?.transmit(frequency, pattern)
             //println("Signal transmis")
         }
     }
